149b1d42eb9640354276574b2f138d3261e20def
lucas cortez <lucas.cortez@w3americas.com>
2024-05-20 00:27:19 -0300

    feat: add PokemonCardController to show Pokémon card details and handle requests related to specific Pokémon cards

---
+++ src/Controller/PokemonCardController.php
@@ -0,0 +1,47 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Service\PokemonCardService;
+use Psr\Cache\InvalidArgumentException;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
+/**
+ * Controller for handling Pokémon card related requests.
+ */
+class PokemonCardController extends AbstractController
+{
+    private PokemonCardService $pokemonCardService;
+
+    public function __construct(PokemonCardService $pokemonCardService)
+    {
+        $this->pokemonCardService = $pokemonCardService;
+    }
+
+    /**
+     * Show details of a specific Pokémon card.
+     *
+     * @param  non-empty-string $id
+     * @return Response
+     */
+    #[Route('/card/{id}', name: 'pokemon_card_detail')]
+    public function detail(string $id): Response
+    {
+        try {
+            $card = $this->pokemonCardService->getPokemonCardById($id);
+        } catch (\RuntimeException $e) {
+            throw new HttpException(Response::HTTP_INTERNAL_SERVER_ERROR, $e->getMessage());
+        } catch (InvalidArgumentException $e) {
+        }
+
+        if (!$card) {
+            throw $this->createNotFoundException('The Pokémon card does not exist');
+        }
+
+        return $this->render('main/detail.html.twig', ['pokemonCard' => $card]);
+    }
+}
