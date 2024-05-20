33c209437fa9cab40a6427dfe7dcb7478f1d7162
lucas cortez <lucas.cortez@w3americas.com>
2024-05-20 00:35:00 -0300

    feat: add ErrorController to handle exceptions and display appropriate error messages

---
+++ src/Controller/ErrorController.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ErrorController extends AbstractController
+{
+    #[Route('/error', name: 'app_error')]
+    public function show(\Throwable $exception): Response
+    {
+        $statusCode = $exception instanceof HttpExceptionInterface
+            ? $exception->getStatusCode() : Response::HTTP_INTERNAL_SERVER_ERROR;
+
+        return $this->render(
+            'error.html.twig',
+            [
+            'status_code' => $statusCode,
+            'status_text' => Response::$statusTexts[$statusCode] ?? 'An error occurred',
+            'exception' => $exception,
+            ]
+        );
+    }
+}
