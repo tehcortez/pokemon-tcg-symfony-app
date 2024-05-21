3aeb874b8c40df48c1337cb2510cacc6198155d0
lucas cortez <lucas.cortez@w3americas.com>
2024-05-21 06:09:00 -0300

    refactor: add logging to ErrorController

--- src/Controller/ErrorController.php
+++ src/Controller/ErrorController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,9 +12,17 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ErrorController extends AbstractController
 {
+    private LoggerInterface $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
     #[Route('/error', name: 'app_error')]
     public function show(\Throwable $exception): Response
     {
+        $this->logger->error($exception->getMessage(), ['exception' => $exception]);
+
         $statusCode = $exception instanceof HttpExceptionInterface
             ? $exception->getStatusCode() : Response::HTTP_INTERNAL_SERVER_ERROR;
 
